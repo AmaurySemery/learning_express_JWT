56c59d6abee432c3d7c66af49e228b192653a8e7
Amaury Semery <amaury.semery@laposte.net>
2024-01-04 11:25:06 +0100

    Importance de l'ordre des routes

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/movies', (req, res) => {
     res.send('Bientôt des films ici même');
 });
 
+app.get('/movies/add', (req, res) => {
+    res.send("Prochainement, un formulaire d'ajout ici");
+});
+
 app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
     res.send(`Film numéro ${id}`);
