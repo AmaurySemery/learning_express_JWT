1fd5894a4246333dfb2f7faec0a8c8249739946d
Amaury Semery <amaury.semery@laposte.net>
2024-01-05 16:48:20 +0100

    Faire une requête à l'aide de axios

--- app.js
+++ app.js
@@ -71,6 +71,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/movie-search', (req, res) => {
+    res.render('movie-search');
+})
+
 app.listen(3000, (req, res) => {
     console.log(`listening on port ${PORT}`);
 });
